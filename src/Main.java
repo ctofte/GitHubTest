34dbefcdb4b6c887cf22abd1eed4e9844a6f1763
ctofte <ctofte@ruc.dk>
2021-09-15 11:23:52 +0200

    Leder efter Gizkongen

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Orale marakeeech - Flexx0 ER KONGEN");
+        System.out.println("Hvor er Gizkongen?");
     }
 }
