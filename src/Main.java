cdc09f9290935979bd78366b178a62c3029b19ba
Bas2904 <bastian.rasch@hotmail.com>
2021-09-15 11:27:58 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Orale marakeeech - Flexx0 ER KONGEN");
         System.out.println("Hvor er Gizkongen?");
+        System.out.println("To pldaser til venstre for Krølle");
     }
 }
