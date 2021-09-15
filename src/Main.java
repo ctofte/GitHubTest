5ebe49d499dae525dddde5628123ba0decb597c8
Bas2904 <bastian.rasch@hotmail.com>
2021-09-15 11:33:58 +0200

    Bas was here

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Orale marakeeech - Flexx0 ER KONGEN");
         System.out.println("Hvor er Gizkongen?");
-        System.out.println("To pldaser til venstre for Krølle");
+        System.out.println("To pladser til venstre for Krølle");
         System.out.println("ss");
+        System.out.println("Nogle der vil med i kantinen? Hilsen Flexx0.");
     }
 }
