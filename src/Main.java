2c0d3209e8460032b2ed886ced114b12003ccf7c
chrgaertner <chrigar@ruc.dk>
2021-09-15 11:36:46 +0200

    svar til bastian

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("To pladser til venstre for Krølle");
         System.out.println("ss");
         System.out.println("Nogle der vil med i kantinen? Hilsen Flexx0.");
+        System.out.println("Når vi lige kan køre programmet kan vi godt");
     }
 }
